bfbe0037a946ffd9862c5511dbfbf1c49b80c7fe
islamibrahiem <islamibrahiem93@gmail.com>
2020-07-22 17:44:13 +0200

    allure report

--- src/test/java/Test/ContactUSinParallel.java
+++ src/test/java/Test/ContactUSinParallel.java
@@ -2,6 +2,10 @@ package Test;
 
 import Pages.ContactUsPage;
 import Pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -15,6 +19,9 @@ public class ContactUSinParallel extends TestBase2{
     String enquirytest = "Test";
 
     @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Contact US")
+    @Link(name = "Contact US Link", url = "https://www.nopcommerce.com/en/contact-us")
     public void insertTheINFtoContactUs() throws InterruptedException {
         homePageObject = new HomePage(getDriver());
         homePageObject.pressOnContactUS();
